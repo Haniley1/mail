63507d4a77f3c323ff789e2cef8a7b1dade71f34
Sergey Nechaev <slow_cheetah@inbox.ru>
2022-08-13 12:25:14 +0700

    [BFT-333] hide reactions in group chats

--- widgets/views/_conversationEntryMenu.php
+++ widgets/views/_conversationEntryMenu.php
@@ -14,12 +14,15 @@ $isOwnMessage = $entry->user->is(Yii::$app->user->getIdentity());
 $lastEntryId = $entry->message->lastEntry->id;
 
 ?>
-<div class="message-reaction">
-    <span class="reaction-1" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"heart"}'>❤️</span>
-    <span class="reaction-2" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"flame"}'>🔥</span>
-    <span class="reaction-3" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"thumb_up"}'>👍</span>
-    <span class="reaction-4" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"crying"}'>😢</span>
-</div>
+<?php if (count($entry->message->users) <= 2) : ?>
+    <div class="message-reaction">
+        <span class="reaction-1" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"heart"}'>❤️</span>
+        <span class="reaction-2" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"flame"}'>🔥</span>
+        <span class="reaction-3" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"thumb_up"}'>👍</span>
+        <span class="reaction-4" data-action-click="mail.ConversationEntry.reaction.react" data-action-params='{"messageEntryId":"<?= $entry->id ?>","type":"crying"}'>😢</span>
+    </div>
+<?php endif; ?>
+
 <?php if(($isOwnMessage && ($lastEntryId == $entry->id)) || ($isOwnMessage && Yii::$app->user->isAdmin())) : ?>
     <li class="edit">
         <?= ModalButton::none('' . Yii::t('custom', 'Редактировать') . '')->cssClass('conversation-edit-button')->load(Url::toEditMessageEntry($entry))->link() ?>
